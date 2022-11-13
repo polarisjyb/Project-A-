53b5b11f0467497a8854c23622f3195e3755a721
Violet Ultra <evia_a@naver.com>
2022-11-13 23:29:37 +0900

    webpack react typescript 설정 테스트

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import Test from "./components/Test";
+
+const App = () => {
+  return <Test/>;
+}
+
+export default App;
---
+++ src/components/Test.tsx
@@ -0,0 +1,4 @@
+const Test = () => {
+  return <h1> 테스트 입니다. </h1>
+};
+export default Test;
