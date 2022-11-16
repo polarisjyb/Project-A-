be67d572d791405547599dc564718514c8f8772d
yeonjujeong0511 <yeonjujeong0511@gmail.com>
2022-11-16 16:53:13 +0900

    router 설정

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,12 @@
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App/App";
-
+import "./index.css";
 const container = document.getElementById("root");
 const root = createRoot(container as Element);
 
-root.render(<App />);
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
