e80d0c4b19b3dc264293ecc4970459614d72490f
yeonjujeong0511 <yeonjujeong0511@gmail.com>
2022-11-16 17:35:38 +0900

    router / outlet 설정

---
+++ src/components/Header2.tsx
@@ -0,0 +1,22 @@
+import { Outlet } from "react-router-dom";
+import styled from "styled-components";
+const HeaderBox = styled.div`
+  width: 1480px;
+  height: 450px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+const Header2 = () => {
+  return (
+    <>
+      <HeaderBox>
+        <div>로고사진</div>
+        <div>검색창</div>
+      </HeaderBox>
+      <Outlet />
+    </>
+  );
+};
+
+export default Header2;
