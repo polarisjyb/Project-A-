3d6e742e0d660c59c66507622df32ffa544fff34
Violet Ultra <evia_a@naver.com>
2022-11-18 17:11:36 +0900

    주가 테이블 레이아웃 생성

---
+++ src/components/youngbin/Layout/StockTable.tsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const StockList = styled.div`
+  width: 1480px;
+  height: 1215px;
+  background: #e4e4e4;
+  border-radius: 20px;
+`
+const StockTable = () => {
+  return (
+    <StockList>
+
+    </StockList>
+  );
+};
+
+export default StockTable;
