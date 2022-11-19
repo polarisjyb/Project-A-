6d8b41295f7938cf8de6f4cb6ecaeeef8336c5df
Violet Ultra <evia_a@naver.com>
2022-11-19 23:00:25 +0900

    test 차원에서 만들었던 json 파일 삭제

--- src/components/youngbin/Layout/Main.tsx
+++ src/components/youngbin/Layout/Main.tsx
@@ -2,7 +2,6 @@ import styled from "styled-components";
 // import Search from "./SearchBox";
 import StockTable from "./StockTable";
 import { Outlet } from "react-router-dom";
-import BookData from "../Layout/Data.json"
 
 const MainBox = styled.div`
   width: 1480px;
