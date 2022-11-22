ead4e58e9b359a1b937d8aadd6e82ac7dd0a352a
Hwayeon Choi <hwayeon.choi11@gmail.com>
2022-11-22 16:45:45 +0900

    axios 시도

--- src/components/hwayeon/Hwayeon.tsx
+++ src/components/hwayeon/Hwayeon.tsx
@@ -1,10 +1,44 @@
-const Hwayeon = () => {
-  return(
-    <div>
-      화연의 전략페이지
-    </div>
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
-  )
+export interface StockObject {
+  close: string;
+  day: string;
+  high: string;
+  low: string;
+  market: string;
+  name: string;
+  open: string;
+  volume: string;
+  [index: number]: any;
+} 
+
+export default () => {
+  const [stockCode, setStockCode] = useState<string>('000250');
+  const [data, setData] = useState<StockObject[]>([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await axios.get(
+          `http://127.0.0.1:5000/${stockCode}`
+        );
+        setData(response.data);
+        console.log(response);
+      } catch(err) {
+        console.log(err);
+      }
+    };
+    getData();
+  }, [stockCode]);
+
+  return (
+    <ul>
+      {data.map((items) => (
+        <>
+          <li key={items.name}>${items.name}</li>
+        </>
+      ))}
+    </ul>
+  );
 }
-
-export default Hwayeon;
