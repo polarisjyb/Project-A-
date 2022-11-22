0adebdf414430feff88ac2a9334a5002733664e3
Hwayeon Choi <hwayeon.choi11@gmail.com>
2022-11-22 19:50:34 +0900

    axios 통신완료

--- src/components/hwayeon/Hwayeon.tsx
+++ src/components/hwayeon/Hwayeon.tsx
@@ -24,21 +24,19 @@ export default () => {
         `http://127.0.0.1:5000/${search}`
       );
       setData(result.data);
-      console.log(result)
-      console.log(result.data[0][0].name);
-      console.log(result.data[1][0].high);
-      console.log(result.data[1][0].low);
-      console.log(result.data[1][0].open);
-      console.log(result.data[1][0].close);
-      console.log(result.data[1][0].volume);
-      console.log(result.data[1][0]);
+      // console.log(result)
+      // console.log(result.data[0][0].name);
+      // console.log(result.data[1][0].high);
+      // console.log(result.data[1][0].low);
+      // console.log(result.data[1][0].open);
+      // console.log(result.data[1][0].close);
+      // console.log(result.data[1][0].volume);
+      // console.log(result.data[1][0]);
     };
     fetchData();
     // 검색 시에만 data fetching을 요구해야 하므로 객체에는 search을 넣음
   }, [search]);
 
-  console.log(data);
-  
   return (
     <>
       <input
@@ -49,16 +47,21 @@ export default () => {
       <button type="button" onClick={() => setSearch(query)}>
         Search
       </button>
-        
-          {/* {data.map((value: any) => {
-            return (
-              <ul key={value}>
-                <li>{value[1][0].close}</li>
-              </ul>
-            )
-            })
-        } */}
-        
+      <div>
+        {data.map && data.map((item: any, index) => {
+          return(
+            <ul key={item}>
+              <li>{item[index].name}</li>
+              <li>{item[index].open}</li>
+              <li>{item[index].high}</li>
+              <li>{item[index].low}</li>
+              <li>{item[index].close}</li>
+              <li>{item[index].volume}</li>
+            </ul>
+          )
+        })
+        }
+      </div>
     </>
   );
 }
