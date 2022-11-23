9053c40dd449d39a3cbcd839b9e5fef3e3126b44
Hwayeon Choi <hwayeon.choi11@gmail.com>
2022-11-23 17:55:28 +0900

    전략 알고리즘 테스트

--- server/analysis.py
+++ server/analysis.py
@@ -1,4 +1,4 @@
-from db import code_to_data, code_to_name;
+from db import code_to_data;
 
 # print(type(code_to_data('000250')))
 # print(code_to_name('000250'))
@@ -9,20 +9,26 @@ def calculate_avg(code):
     for i in range (len(datas)):
         avg = (datas[i]['open'] + datas[i]['high'] + datas[i]['low'] + datas[i]['close']) / 4 
         temp.append(avg)
-    print(sum(temp)/100)
-        
-# calculate_avg('000250')
+    return (sum(temp)/100)
+  
+  
+def today_price(code):
+    datas = code_to_data(code)
+    return datas[0]['open']    
 
-averge_value = calculate_avg('000250')
 
-def proposal_result(today):
-    high = averge_value + (averge_value * 0.03)
-    low = averge_value - (averge_value * 0.03)
+def proposal_result(code):
+    averge_value = calculate_avg(code)
+    today = today_price(code)
+    print(averge_value)
+    print(today)
+    high = averge_value + (averge_value * 0.01)
+    low = averge_value - (averge_value * 0.05)
     if today >= high:
-        print("매수")
+        print("매수") 
     if today <= low:
         print("매도")
-    else :
-        print("판정이 어렵습니다.")
-        
-proposal_result(45800)
+    else:
+        print("판단이 어렵습니다.")
+  
+proposal_result('003300')
