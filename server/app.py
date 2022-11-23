e4f32639627678744b4de0535a6f5655d9e6a185
yeonjujeong0511 <yeonjujeong0511@gmail.com>
2022-11-23 17:57:43 +0900

    코드별 주식 DB

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_cors import CORS
-from db import code_to_data, code_to_name, all_company_name, companylist_rank
+from db import code_to_data, code_to_name, all_company_name, companylist_rank, company_info
 
 app = Flask(__name__)
 CORS(app)
@@ -29,6 +29,12 @@ def allCompanyList():
 def companylistRank():
     data = companylist_rank()
 
+    return data
+
+
+@app.route('/<code>')
+def code():
+    data = company_info()
     return data
 
 
