1fc5ae9b7b3aad86c9e1ef5455a01abb934ba731
Violet Ultra <evia_a@naver.com>
2022-11-29 17:33:33 +0900

    데이터 가공 및 연산 처리 파일(매수 or 매도 결정)

--- server/analysis.py
+++ server/analysis.py
@@ -1,11 +1,9 @@
 from db import code_to_data;
 
-# print(type(code_to_data('000250')))
-# print(code_to_name('000250'))
-
 def calculate_avg(code):
     datas = code_to_data(code)
     temp = [];
+    # print(len(datas))
     for i in range (len(datas)):
         avg = (datas[i]['open'] + datas[i]['high'] + datas[i]['low'] + datas[i]['close']) / 4 
         temp.append(avg)
@@ -20,15 +18,13 @@ def today_price(code):
 def proposal_result(code):
     averge_value = calculate_avg(code)
     today = today_price(code)
-    print(averge_value)
-    print(today)
+    # print(averge_value)
+    # print(today)
     high = averge_value + (averge_value * 0.01)
-    low = averge_value - (averge_value * 0.05)
+    # low = averge_value - (averge_value * 0.01)
     if today >= high:
-        print("매수") 
-    if today <= low:
-        print("매도")
+        return("매수") 
     else:
-        print("판단이 어렵습니다.")
+        return("매도")
 
-proposal_result('003300')
+print(proposal_result('003100')) 
