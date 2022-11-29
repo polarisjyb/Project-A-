e5fea94848184e3e94d25180b1ae3402262696ef
Violet Ultra <evia_a@naver.com>
2022-11-29 17:48:04 +0900

    메인 페이지 StockTable height 값 조정

--- src/components/youngbin/Layout/StockTable.tsx
+++ src/components/youngbin/Layout/StockTable.tsx
@@ -6,7 +6,6 @@ import { Link, Navigate } from "react-router-dom";
 
 const StockList = styled.div`
   width: 1480px;
-  height: 1215px;
   background: #e4e4e4;
   border-radius: 20px;
   font-family: "GmarketSansMedium";
